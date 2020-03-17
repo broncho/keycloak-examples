9335ffdef4061d71b12a481741d5fea520cdcada
secondriver <secondriver@yeah.net>
2020-03-17 18:18:27 +0800

    完善client_credentials示例

--- client_credentials/resource-app-parent/resource-app-client/src/main/java/com/sinosun/keycloak/client/AppClient.java
+++ client_credentials/resource-app-parent/resource-app-client/src/main/java/com/sinosun/keycloak/client/AppClient.java
@@ -1,10 +1,18 @@
 package com.sinosun.keycloak.client;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import org.keycloak.adapters.springboot.client.KeycloakSecurityContextClientRequestInterceptor;
 import org.keycloak.authorization.client.AuthzClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.RestTemplate;
@@ -16,33 +24,59 @@ import java.util.Collections;
  * Author: secondriver
  * Created: 2020/3/16
  */
+@RunWith(JUnit4.class)
 public class AppClient {
     
+    private final Logger logger = LoggerFactory.getLogger(AppClient.class);
     
-    public static void main(String[] args) {
-        RestTemplate restTemplate = new RestTemplateBuilder()
-                .customizers(new ClientRestTemplateCustomizer())
-                .build();
-        
-        
-        
-        String oneService = "http://127.0.0.1:9100";
-        
-        String oneValue = restTemplate.getForObject(oneService + "/one/index", String.class);
-        System.out.println(oneValue);
-        
-        String twoValue = restTemplate.getForObject(oneService + "/one/greeting?name=jack", String.class);
-        System.out.println(twoValue);
+    private RestTemplate restTemplate = new RestTemplateBuilder()
+            .customizers(new ClientRestTemplateCustomizer())
+            .build();
+    
+    private String oneService = "http://127.0.0.1:9100";
+    
+    private String twoService = "http://127.0.0.1:9200";
+   
+    
+    
+    @Test
+    public void testOneIndex() {
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(oneService + "/one/index", String.class);
+        Assert.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        logger.info("/one/index ->  {}", responseEntity.getBody());
+    }
+    
+    @Test
+    public void testOneGreeting() {
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(oneService + "/one/greeting?name=one", String.class);
+        Assert.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        logger.info("/one/greeting?name=one ->  {}", responseEntity.getBody());
     }
     
     
+    @Test
+    public void testTwoIndex() {
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(twoService + "/two/index", String.class);
+        Assert.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        logger.info("/two/index ->  {}", responseEntity.getBody());
+    }
+    
+    @Test
+    public void testTwoGreeting() {
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(twoService + "/two/greeting?name=two", String.class);
+        Assert.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        logger.info("/two/greeting?name=two ->  {}", responseEntity.getBody());
+    }
+    
     static class ServiceContext {
+        
+        private static final Logger LOGGER = LoggerFactory.getLogger(ServiceContext.class);
         
         private static AuthzClient authzClient = AuthzClient.create();
         
-        public static String getToken() {
+        static String getToken() {
             String token = authzClient.obtainAccessToken().getToken();
-            System.out.println("accessToken:" + token);
+            LOGGER.info("Obtain access token: {}", token);
             return token;
         }
     }
